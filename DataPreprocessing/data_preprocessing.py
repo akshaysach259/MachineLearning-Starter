781c6c27118292a241a44d78a2d70d2436ece094
Akshay <akshaysach259@gmail.com>
2021-07-28 11:58:44 +0530

    Data Preprocessing - test_train_split and Feature Scaling

--- DataPreprocessing/data_preprocessing.py
+++ DataPreprocessing/data_preprocessing.py
@@ -8,7 +8,6 @@
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer 
  
 dataset = pd.read_csv("Data.csv")
@@ -28,7 +27,19 @@ X[:,0]=labelencoder_X.fit_transform(X[:,0])
 ct = ColumnTransformer([("Country",OneHotEncoder(),[0])],remainder="passthrough")
 X = ct.fit_transform(X)
 
+#Encoding from stering to numeric
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 
+#Seprating Training data and tesing data
 from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.3)
+
+
+#Scaling of data
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
